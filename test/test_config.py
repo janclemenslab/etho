b32c666ee5e4a2e75cb469279ce6b5f62fa2066a
postpop <clemensjan@googlemail.com>
2018-11-12 15:37:29 +0100

    remove duplicates

--- test/test_config.py
+++
@@ -1,8 +0,0 @@
-from ethomaster import config
-from ethomaster.utils.config import readconfig
-
-print(config)
-
-print(readconfig('test/test_config.ini'))
-print(readconfig('test/test_config.yaml'))
-print(type(readconfig('test/test_config.yaml')))
