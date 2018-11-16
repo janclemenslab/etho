32dac2a6882f97fb1bf16054652d0477634f23c0
Jan Clemens <clemensjan@googlemail.com>
2018-11-16 10:08:21 +0100

    updated to work on mic with yml config and prot

--- ethomaster/head/clientcaller.py
+++ ethomaster/head/clientcaller.py
@@ -6,6 +6,9 @@ from itertools import cycle
 
 from ethomaster import config
 from ethomaster.head.ZeroClient import ZeroClient
+from ethomaster.utils.config import readconfig
+from ethomaster.utils.sound import parse_table, load_sounds, build_playlist
+
 from ethoservice.SndZeroService import SND
 from ethoservice.CamZeroService import CAM
 from ethoservice.ThuZeroService import THU
@@ -14,18 +17,15 @@ from ethoservice.OptZeroService import OPT
 from ethoservice.DAQZeroService import DAQ
 from ethoservice.PTGZeroService import PTG
 
-from ethomaster.utils.sound import *
-from ethomaster.utils.config import readconfig
-
 
 def clientcaller(ip_address, playlistfile, protocolfile, filename=None):
     # load config/protocols
     prot = readconfig(protocolfile)
     print(prot)
-    maxduration = int(prot['NODE']['maxduration'])
+    maxduration = prot['NODE']['maxduration']
     user_name = prot['NODE']['user']
     folder_name = prot['NODE']['folder']
-    SER = 'pickle'
+    SER = prot['NODE']['serializer']
 
     # unique file name for video and node-local logs
     if filename is None:
@@ -99,14 +99,12 @@ def clientcaller(ip_address, playlistfile, protocolfile, filename=None):
         opt.start()
 
     if 'THUA' in prot['NODE']['use_services']:
-        thua_server_name = 'python -m {0}'.format(THUA.__module__)
-        thua_service_port = THUA.SERVICE_PORT
+        thua_server_name = 'python -m {0}'.format(THUA.__module__, SER)
         print([THUA.SERVICE_PORT, THUA.SERVICE_NAME])
 
         thua = ZeroClient("{0}@{1}".format(user_name, ip_address), 'thuarduino')
-        # print(thua.start_server(thua_server_name, folder_name, warmup=1))
         subprocess.Popen(thua_server_name, creationflags=subprocess.CREATE_NEW_CONSOLE)
-        thua.connect("tcp://{0}:{1}".format(ip_address, thua_service_port))
+        thua.connect("tcp://{0}:{1}".format(ip_address, THUA.SERVICE_PORT))
         print('done')
         print(prot['THUA'])
         thua.setup(prot['THUA']['port'], float(prot['THUA']['interval']), maxduration + 10)
@@ -114,14 +112,12 @@ def clientcaller(ip_address, playlistfile, protocolfile, filename=None):
         thua.start()
 
     if 'PTG' in prot['NODE']['use_services']:
-        ptg_server_name = 'python -m {0}'.format(PTG.__module__)
-        ptg_service_port = PTG.SERVICE_PORT
+        ptg_server_name = 'python -m {0}'.format(PTG.__module__, SER)
         print([PTG.SERVICE_PORT, PTG.SERVICE_NAME])
 
         ptg = ZeroClient("{0}@{1}".format(user_name, ip_address), 'ptgcam')
-        # print(ptg.start_server(ptg_server_name, folder_name, warmup=1))
         subprocess.Popen(ptg_server_name, creationflags=subprocess.CREATE_NEW_CONSOLE)
-        ptg.connect("tcp://{0}:{1}".format(ip_address, ptg_service_port))
+        ptg.connect("tcp://{0}:{1}".format(ip_address, PTG.SERVICE_PORT))
         print('done')
         ptg.setup('{0}/{1}/{1}'.format(dirname, filename), maxduration + 10, prot['PTG'])
         ptg.init_local_logger('{0}/{1}/{1}_ptg.log'.format(dirname, filename))
@@ -130,16 +126,12 @@ def clientcaller(ip_address, playlistfile, protocolfile, filename=None):
 
     if 'DAQ' in prot['NODE']['use_services']:
         daq_server_name = 'python -m {0} {1}'.format(DAQ.__module__, SER)
-        daq_service_port = DAQ.SERVICE_PORT
 
         fs = int(prot['DAQ']['samplingrate'])
-        shuffle_playback = eval(prot['DAQ']['shuffle'])
-        # load playlist, sounds, and enumerate play order
-        playlist = pd.read_table(playlistfile, dtype=None, delimiter='\t')
+        shuffle_playback = prot['DAQ']['shuffle']
+        playlist = parse_table(playlistfile)
         sounds = load_sounds(playlist, fs, attenuation=config['ATTENUATION'],
-                             LEDamp=prot['DAQ']['ledamp'], stimfolder=config['HEAD']['stimfolder'],
-                             mirrorsound=bool(int(prot['NODE'].get('mirrorsound', 1))),
-                             cast2int=False, aslist=False)
+                             LEDamp=prot['DAQ']['ledamp'], stimfolder=config['HEAD']['stimfolder'])
         playlist_items, totallen = build_playlist(sounds, maxduration, fs, shuffle=shuffle_playback)
         if maxduration == -1:
             print(f'setting maxduration from playlist to {totallen}.')
@@ -148,25 +140,19 @@ def clientcaller(ip_address, playlistfile, protocolfile, filename=None):
         else:
             playlist_items = cycle(playlist_items)
 
-        if not isinstance(prot['DAQ']['analog_chans_in'], list):
-            prot['DAQ']['analog_chans_in'] = [prot['DAQ']['analog_chans_in']]
-        if not isinstance(prot['DAQ']['analog_chans_out'], list):
-            prot['DAQ']['analog_chans_out'] = [prot['DAQ']['analog_chans_out']]
-
         daq_save_filename = '{0}/{1}/{1}_daq_test.h5'.format(dirname, filename)
         print([DAQ.SERVICE_PORT, DAQ.SERVICE_NAME])
         daq = ZeroClient("{0}@{1}".format(user_name, ip_address), 'nidaq', serializer=SER)
         # print(daq.start_server(daq_server_name, folder_name, warmup=1))
         subprocess.Popen(daq_server_name, creationflags=subprocess.CREATE_NEW_CONSOLE)
-        daq.connect("tcp://{0}:{1}".format(ip_address, daq_service_port))
+        daq.connect("tcp://{0}:{1}".format(ip_address, DAQ.SERVICE_PORT))
         print('done')
         print('sending sound data to {0} - may take a while.'.format(ip_address))
         # daq.setup(daq_save_filename, sounds, playlist.to_msgpack(), playlist_items, maxduration, fs, prot['DAQ'])
-        daq.setup(daq_save_filename, playlist_items, maxduration, fs, eval(prot['DAQ'].get('display', 'False')),
+        daq.setup(daq_save_filename, playlist_items, maxduration, fs, prot['DAQ'].get('display', 'False'),
                   analog_chans_out=prot['DAQ'].get('analog_chans_out', []),
                   analog_chans_in=prot['DAQ'].get('analog_chans_in', []),
                   analog_data_out=sounds)
-
 
         daq.init_local_logger('{0}/{1}/{1}_daq.log'.format(dirname, filename))
         daq.start()
