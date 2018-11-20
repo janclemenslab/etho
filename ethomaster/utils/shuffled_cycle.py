962a64f35612c2b2647b528d12729586d5f9646a
postpop <clemensjan@googlemail.com>
2018-11-20 16:41:38 +0100

    check args

--- ethomaster/utils/shuffled_cycle.py
+++ ethomaster/utils/shuffled_cycle.py
@@ -11,28 +11,30 @@ class shuffled_cycle(cycle):
     cycle('ABCD', shuffle='full')  --> ACCBBDBDCABA ...
     """
 
-    def __init__(self, it: Iterable, shuffle: str=None):
+    def __init__(self, it: Iterable, shuffle: str='block'):
         """.
 
         Arguments:
             it: Iterable
             shuffle: None, 'block', 'full'
         """
+        allowed_shuffles = ['block', 'full']
+        if shuffle not in allowed_shuffles:
+            raise ValueError(f'shuffle should be one of {allowed_shuffles}.')
         super(shuffled_cycle, self).__init__()
         self._shuffle = shuffle
         self._it = list(it)
         self._pos = -1
-
 
     def __next__(self):
         """Return next item in iterator."""
         self._pos = (self._pos+1) % len(self._it)  # wrap index
         if self._shuffle is 'block':
             if self._pos==0:
-                random.shuffle(self._it)  # in-place shuffle
+                random.shuffle(self._it)  # in-place shuffle the iterator
             idx = self._pos
         elif self._shuffle is 'full':
             idx = random.randint(0, len(self._it)-1)  # since randint bounds are inclusive
-        else:
-            idx = self._pos
+        # else:
+        #     idx = self._pos
         return self._it[idx]
