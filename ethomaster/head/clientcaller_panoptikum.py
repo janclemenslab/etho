2ba5fa715416e349c37ab4a5dc69f20bf9465c3f
ncb <ncb@eni.uni-goettingen.de>
2020-08-21 08:58:30 +0200

    use new callbacks

--- ethomaster/head/clientcaller_panoptikum.py
+++ ethomaster/head/clientcaller_panoptikum.py
@@ -7,7 +7,7 @@ from itertools import cycle
 
 from ethomaster import config
 from ethomaster.head.ZeroClient import ZeroClient
-from ethomaster.utils.config import readconfig
+from ethomaster.utils.config import readconfig, undefaultify
 from ethomaster.utils.sound import parse_table, load_sounds, build_playlist
 
 from ethoservice.SndZeroService import SND
@@ -15,9 +15,9 @@ from ethoservice.CamZeroService import CAM
 from ethoservice.ThuZeroService import THU
 from ethoservice.ThuAZeroService import THUA
 from ethoservice.OptZeroService import OPT
-from ethoservice.DAQZeroService import DAQ
+from ethoservice.DAQZeroService_new import DAQ
 from ethoservice.PTGZeroService import PTG
-from ethoservice.SPNZeroService import SPN
+from ethoservice.SPNZeroService_new import SPN
 
 
 def clientcaller(ip_address, playlistfile, protocolfile, filename=None):
@@ -44,10 +44,10 @@ def clientcaller(ip_address, playlistfile, protocolfile, filename=None):
         ptg_server_name = f'{python_exe} -m {SPN.__module__} {SER}'
         print([SPN.SERVICE_PORT, SPN.SERVICE_NAME])
         ptg = ZeroClient("{0}@{1}".format(user_name, ip_address), 'spn', serializer=SER)
-        subprocess.Popen(ptg_server_name, creationflags=subprocess.CREATE_NEW_CONSOLE)
+        subprocess.Popen(ptg_server_name, creationflags=subprocesEs.CREATE_NEW_CONSOLE)
         ptg.connect("tcp://{0}:{1}".format(ip_address, PTG.SERVICE_PORT))
         print('done')
-        cam_params = dict(prot['SPN'])
+        cam_params = undefaultify(prot['SPN'])
         ptg.setup('{0}/{1}/{1}'.format(dirname, filename), maxduration + 10, cam_params)
         ptg.init_local_logger('{0}/{1}/{1}_spn.log'.format(dirname, filename))
         
@@ -59,7 +59,7 @@ def clientcaller(ip_address, playlistfile, protocolfile, filename=None):
         subprocess.Popen(ptg_server_name, creationflags=subprocess.CREATE_NEW_CONSOLE)
         ptg2.connect("tcp://{0}:{1}".format(ip_address, port))
         print('done')
-        cam_params = dict(prot['SPN_ZOOM'])
+        cam_params = undefaultify(prot['SPN_ZOOM'])
         ptg2.setup('{0}/{1}/{1}_zoom'.format(dirname, filename), maxduration + 10, cam_params)
         ptg2.init_local_logger('{0}/{1}/{1}_spnzoom.log'.format(dirname, filename))   
         ptg2.start()
@@ -67,7 +67,7 @@ def clientcaller(ip_address, playlistfile, protocolfile, filename=None):
     if 'SPN' in prot['NODE']['use_services']:
         ptg.start()
 
-    time.sleep(5)
+    t0 = time.time()
 
     if 'DAQ' in prot['NODE']['use_services']:
         daq_server_name = f'{python_exe} -m {DAQ.__module__} {SER}'
@@ -116,6 +116,7 @@ def clientcaller(ip_address, playlistfile, protocolfile, filename=None):
         print('done')
         print('sending sound data to {0} - may take a while.'.format(ip_address))
 
+        daq_params = undefaultify(prot['DAQ'])
         daq.setup(daq_save_filename, playlist_items, playlist,
               maxduration, fs,
               display=prot['DAQ']['display'],
@@ -127,8 +128,15 @@ def clientcaller(ip_address, playlistfile, protocolfile, filename=None):
               digital_chans_out=prot['DAQ']['digital_chans_out'],
               analog_data_out=analog_data,
               digital_data_out=digital_data,
-              metadata={'analog_chans_in_info': prot['DAQ']['analog_chans_in_info']})
+              metadata={'analog_chans_in_info': prot['DAQ']['analog_chans_in_info']},
+              params=daq_params)
         daq.init_local_logger('{0}/{1}/{1}_daq.log'.format(daq_save_folder, filename))
+    
+    if 'DAQ' in prot['NODE']['use_services']:
+        while time.time() - t0 < 5:
+            time.sleep(1)
+
+
         daq.start()
         logging.info('DAQ started')
 
@@ -137,7 +145,7 @@ def clientcaller(ip_address, playlistfile, protocolfile, filename=None):
 
 if __name__ == '__main__':
     ip_address = 'localhost'
-    #protocolfilename = 'C:/Users/ncb/ethoconfig/protocols/panoptikum_zoom-only_5min.yml'
-    protocolfilename = 'C:/Users/ncb/ethoconfig/protocols/panoptikum_zoom-zoom_5min.yml'
+
+    protocolfilename = 'C:/Users/ncb/ethoconfig/protocols/panoptikum_1min_TEST.yml'
     playlistfilename = 'C:/Users/ncb/ethoconfig/playlists/0 silence.txt'
     clientcaller(ip_address, playlistfilename, protocolfilename)
