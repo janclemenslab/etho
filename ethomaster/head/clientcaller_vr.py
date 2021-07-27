6b42970e43a0fcb419a9ea8e25e4062754be9db7
Jan Clemens <clemensjan@googlemail.com>
2021-07-27 15:22:58 +0200

    re-orga caller

--- ethomaster/head/clientcaller_vr.py
+++ ethomaster/head/clientcaller_vr.py
@@ -6,6 +6,7 @@ import subprocess
 import logging
 from itertools import cycle
 import defopt
+from pprint import pprint
 
 from ethomaster import config
 from ethomaster.head.ZeroClient import ZeroClient
@@ -21,9 +22,9 @@ from ethoservice.DLPZeroService import DLP
 
 def clientcaller(ip_address, playlistfile, protocolfile, filename=None):
     # load config/protocols
-    print(config)
+    pprint(config)
     prot = readconfig(os.path.join(config['HEAD']['protocolfolder'], protocolfile))
-    print(prot)
+    pprint(undefaultify(prot))
     maxduration = prot['NODE']['maxduration']
     user_name = prot['NODE']['user']
     folder_name = prot['NODE']['folder']
@@ -34,13 +35,14 @@ def clientcaller(ip_address, playlistfile, protocolfile, filename=None):
         filename = '{0}-{1}'.format(ip_address, time.strftime('%Y%m%d_%H%M%S'))
     dirname = prot['NODE']['savefolder']
     print(filename)
-    
-    if 'THUA' in prot['NODE']['use_services']:
-        thua = THUA.make(SER, user_name, ip_address, folder_name, python_exe)
-        print(prot['THUA'])
-        thua.setup(prot['THUA']['port'], prot['THUA']['interval'], maxduration + 10)
-        thua.init_local_logger('{0}/{1}/{1}_thu.log'.format(dirname, filename))
-        thua.start()
+
+    if 'DLP' in prot['NODE']['use_services']:
+        dlp_save_filename = '{0}/{1}/{1}'.format(dirname, filename)
+
+        dlp = DLP.make(SER, user_name, ip_address, folder_name, python_exe)
+        dlp_params = undefaultify(prot['DLP'])
+        dlp.setup(maxduration + 10, dlp_save_filename, params=dlp_params)
+        dlp.init_local_logger('{0}/{1}/{1}_dlp.log'.format(dirname, filename))
 
     if 'SPN' in prot['NODE']['use_services']:
         ptg = GCM.make(SER, user_name, ip_address, folder_name, python_exe)
@@ -50,20 +52,53 @@ def clientcaller(ip_address, playlistfile, protocolfile, filename=None):
         ptg.start()
         # time.sleep(5)
 
+
     if 'BLT' in prot['NODE']['use_services']:
         blt = BLT.make(SER, user_name, ip_address, folder_name, python_exe)
         blt_params = undefaultify(prot['BLT'])
         blt.setup('{0}/{1}/{1}'.format(dirname, filename), maxduration + 10, blt_params)
         blt.init_local_logger('{0}/{1}/{1}_blt.log'.format(dirname, filename))
         blt.start()
-            
-    if 'DLP' in prot['NODE']['use_services']:
-        dlp = DLP.make(SER, user_name, ip_address, folder_name, python_exe)
-        dlp_params = undefaultify(prot['DLP'])
-        dlp.setup(maxduration + 10, params=dlp_params)
-        dlp.init_local_logger('{0}/{1}/{1}_dlp.log'.format(dirname, filename))
-        dlp.start()
-            
+
+
+    # if 'SPN2' in prot['NODE']['use_services']:
+    #     ptg2 = GCM.make(SER, user_name, ip_address, folder_name, python_exe, port=4247)
+    #     cam_params = undefaultify(prot['SPN2'])
+    #     ptg2.setup('{0}/{1}/{1}_2'.format(dirname, filename), maxduration + 10, cam_params)
+    #     ptg2.init_local_logger('{0}/{1}/{1}_spn2.log'.format(dirname, filename))
+    #     ptg2.start()
+
+
+
+    # if 'SPN' in prot['NODE']['use_services']:
+    #     ptg_server_name = f'{python_exe} -m {GCM.__module__} {SER}'
+    #     print([GCM.SERVICE_PORT, GCM.SERVICE_NAME])
+    #     ptg = ZeroClient("{0}@{1}".format(user_name, ip_address), 'spn', serializer=SER)
+    #     subprocess.Popen(ptg_server_name, creationflags=subprocess.CREATE_NEW_CONSOLE)
+    #     ptg.connect("tcp://{0}:{1}".format(ip_address, GCM.SERVICE_PORT))
+    #     print('done')
+    #     cam_params = undefaultify(prot['SPN'])
+    #     ptg.setup('{0}/{1}/{1}'.format(dirname, filename), maxduration + 10, cam_params)
+    #     ptg.init_local_logger('{0}/{1}/{1}_spn.log'.format(dirname, filename))
+    #     ptg.start()
+    #     # services.append(ptg)
+
+    if 'SPN2' in prot['NODE']['use_services']:
+        port = 4247# 4247  # use custom port so we can start two SPN instances
+        ptg_server_name = f'{python_exe} -m {GCM.__module__} {SER} {port}'
+        print([port, GCM.SERVICE_NAME])
+        ptg2 = ZeroClient("{0}@{1}".format(user_name, ip_address), 'spnzoom', serializer=SER)
+        subprocess.Popen(ptg_server_name, creationflags=subprocess.CREATE_NEW_CONSOLE)
+        ptg2.connect("tcp://{0}:{1}".format(ip_address, port))
+        print('done')
+        cam_params = undefaultify(prot['SPN2'])
+        print(cam_params)
+        ptg2.setup('{0}/{1}/{1}_ball'.format(dirname, filename), maxduration + 10, cam_params)
+        ptg2.init_local_logger('{0}/{1}/{1}_ball_spn.log'.format(dirname, filename))
+        ptg2.start()
+        # services.append(ptg2)
+
+
     if 'DAQ' in prot['NODE']['use_services']:
         if prot['DAQ']['run_locally']:
             print('   Running DAQ job locally.')
@@ -100,7 +135,7 @@ def clientcaller(ip_address, playlistfile, protocolfile, filename=None):
         else:
             digital_data = None
             analog_data = sounds
-        daq_save_filename = '{0}/{1}/{1}_daq.h5'.format(daq_save_folder, filename)
+        daq_save_filename = '{0}/{1}/{1}'.format(daq_save_folder, filename)
         daq = DAQ.make(SER, user_name, ip_address, folder_name, python_exe)
         print('sending sound data to {0} - may take a while.'.format(ip_address))
 
@@ -115,16 +150,29 @@ def clientcaller(ip_address, playlistfile, protocolfile, filename=None):
               digital_chans_out=prot['DAQ']['digital_chans_out'],
               analog_data_out=analog_data,
               digital_data_out=digital_data,
-              metadata={'analog_chans_in_info': prot['DAQ']['analog_chans_in_info']})
+              metadata={'analog_chans_in_info': prot['DAQ']['analog_chans_in_info']},
+              params=undefaultify(prot['DAQ']))
         daq.init_local_logger('{0}/{1}/{1}_daq.log'.format(daq_save_folder, filename))
         daq.start()
         logging.info('DAQ started')
+        print('DAQ started')
+
+    if 'DLP' in prot['NODE']['use_services']:
+        dlp.start()
+        logging.info('DLP started')
+        print('DLP started')
 
     print('quitting now - protocol will stop automatically on {0}'.format(ip_address))
 
 
+
+# def cli(protocolfilename: str = 'test_vr.yml', playlistfilename: str = 'ethoconfig/playlists/sine_short.txt'):
+#     ip_address = 'localhost'
+#     clientcaller(ip_address, playlistfilename, protocolfilename)
+
+
 if __name__ == '__main__':
     ip_address = 'localhost'
-    protocolfilename = 'test_vr.yml'
-    playlistfilename = 'ethoconfig/playlists/sine_short.txt'
+    protocolfilename = 'test_new_runners.yml'
+    playlistfilename = '../ethoconfig/playlists/sine.txt'
     clientcaller(ip_address, playlistfilename, protocolfilename)
