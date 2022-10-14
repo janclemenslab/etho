74f52cd3f0d577537f16f20e7864782250776495
Jan Clemens <clemensjan@googlemail.com>
2022-10-14 21:14:33 +0200

    v0.6.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='ethodrome',
-      version='0.6.1',
+      version='0.6.2',
       description='ethodrome',
       url='http://github.com/janclemenslab/ethodrome',
       author='Jan Clemens',
