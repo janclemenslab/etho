9544f30b46ed756062258f15733c2961b8462838
Jan Clemens <clemensjan@googlemail.com>
2022-10-31 11:34:20 +0100

    version in __init__.py

--- setup.py
+++ setup.py
@@ -1,7 +1,27 @@
 from setuptools import setup, find_packages
+import codecs
+import re
+import os
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*parts):
+    with codecs.open(os.path.join(here, *parts), 'r') as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
 
 setup(name='ethodrome',
-      version='0.8.1',
+      version=find_version("src/etho/__init__.py"),
       description='ethodrome',
       url='http://github.com/janclemenslab/ethodrome',
       author='Jan Clemens',
--- src/etho/__init__.py
+++ src/etho/__init__.py
@@ -1,3 +1,4 @@
+__version__ = '0.9.0'
 # load global config on import
 from .utils.config import readconfig
 config = readconfig()
