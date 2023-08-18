c322d6ba30e173091d8f201c2e6ff8f017e522a2
postpop <clemensjan@googlemail.com>
2023-08-18 18:51:12 +0200

    enable custom ports and device names

--- src/etho/services/DAQZeroService.py
+++ src/etho/services/DAQZeroService.py
@@ -34,6 +34,7 @@ class DAQ(BaseZeroService):
         fs: int = 10000,
         display=False,
         realtime=False,
+        dev_name='Dev1',
         nb_inputsamples_per_cycle=None,
         clock_source=None,
         analog_chans_out: Optional[Sequence[str]] = None,
@@ -87,7 +88,7 @@ class DAQ(BaseZeroService):
 
         # ANALOG OUTPUT
         if self.analog_chans_out:
-            self.taskAO = IOTask(cha_name=self.analog_chans_out, rate=fs, clock_source=clock_source)
+            self.taskAO = IOTask(dev_name=dev_name, cha_name=self.analog_chans_out, rate=fs, clock_source=clock_source)
             if analog_data_out[0].shape[-1] is not len(self.analog_chans_out):
                 raise ValueError(
                     f"Number of analog output channels ({len(self.analog_chans_out)}) does not match the number of channels in the sound files ({analog_data_out[0].shape[-1]})."
@@ -100,7 +101,7 @@ class DAQ(BaseZeroService):
                 self.taskAO.DisableStartTrig()
         # DIGITAL OUTPUT
         if self.digital_chans_out:
-            self.taskDO = IOTask(cha_name=self.digital_chans_out, rate=fs, clock_source=clock_source)
+            self.taskDO = IOTask(dev_name=dev_name, cha_name=self.digital_chans_out, rate=fs, clock_source=clock_source)
             play_order_new = copy.deepcopy(play_order)
             self.taskDO.data_gen = data_playlist(digital_data_out, play_order_new, name="DO")
             if clock_source is None:
@@ -110,6 +111,7 @@ class DAQ(BaseZeroService):
         # ANALOG INPUT
         if self.analog_chans_in:
             self.taskAI = IOTask(
+                dev_name=dev_name, 
                 cha_name=self.analog_chans_in,
                 rate=fs,
                 nb_inputsamples_per_cycle=nb_inputsamples_per_cycle,
@@ -121,26 +123,32 @@ class DAQ(BaseZeroService):
             self.callbacks = []
             if metadata is None:
                 metadata = {}
-            attrs = {"rate": fs, "analog_chans_in": analog_chans_in, **metadata}
+            attrs = {
+                "rate": fs,
+                "analog_chans_in": analog_chans_in,
+                "analog_chans_out": analog_chans_out,
+                "digital_chans_out": digital_chans_out,
+                **metadata,
+            }                
             common_task_kwargs = {
                 "file_name": self.savefilename,
                 "nb_inputsamples_per_cycle": nb_inputsamples_per_cycle,
                 "nb_analog_chans_in": len(analog_chans_in),
                 "attrs": attrs,
             }
+            if 'callbacks' in params and params['callbacks']:
+                for cb_name, cb_params in params["callbacks"].items():
+                    if cb_params is not None:
+                        task_kwargs = {**common_task_kwargs, **cb_params}
+                    else:
+                        task_kwargs = common_task_kwargs
 
-            for cb_name, cb_params in params["callbacks"].items():
-                if cb_params is not None:
-                    task_kwargs = {**common_task_kwargs, **cb_params}
-                else:
-                    task_kwargs = common_task_kwargs
-
-                self.taskAI.data_rec.append(callbacks[cb_name].make_concurrent(task_kwargs=task_kwargs))
+                    self.taskAI.data_rec.append(callbacks[cb_name].make_concurrent(task_kwargs=task_kwargs))
 
         if self.duration > 0:  # if zero, will stop when nothing is to be outputted
             self._thread_timer = threading.Timer(self.duration, self.finish, kwargs={"stop_service": True})
         self.status = "initialized"
-
+        
         self.info: Dict[str, Dict[str, Any]] = dict()
         self.info["job"]: Dict[str, Any] = {
             "sample rate": f"{self.fs}Hz",
@@ -270,7 +278,13 @@ if __name__ == "__main__":
         ser = sys.argv[1]
     else:
         ser = "default"
+
+    if len(sys.argv) > 2:
+        port = sys.argv[2]
+    else:
+        port = DAQ.SERVICE_PORT
+
     logging.info("Starting DAQ service")
     s = DAQ(serializer=ser)  # expose class via zerorpc
-    s.bind("tcp://0.0.0.0:{0}".format(DAQ.SERVICE_PORT))  # broadcast on all IPs
+    s.bind("tcp://0.0.0.0:{0}".format(port))  # broadcast on all IPs
     s.run()
