7f315dcb23157b887fa75d5226b46a2772eb3ea4
Jan Clemens <clemensjan@googlemail.com>
2024-02-01 21:26:52 +0100

    print with rich, set correct python exe

--- src/etho/cli.py
+++ src/etho/cli.py
@@ -5,6 +5,8 @@ import importlib
 import yaml
 from pathlib import Path
 import pandas as pd
+import rich
+import sys
 
 logger = logging.getLogger(__name__)
 
@@ -13,6 +15,7 @@ try:
 except Exception as e:
     logging.error(e)
     client = None
+
 
 def version(*, debug: bool = False):
     """Displays system, version, and hardware info.
@@ -85,64 +88,94 @@ def init():
     """Initializes config files and folders."""
     home = Path.home()
 
-    cfg = {'GENERAL': {'user': 'ncb', 'folder': str(home), 'save_directory': str(home / 'data')},
-           'HEAD': {},
-           'ATTENUATION': {-1: 1,  0: 1,100: 1,150: 1, 200: 1, 250: 1, 300: 1, 350: 1, 400: 1, 450: 1, 500: 1, 600: 1, 700: 1, 800: 1, 900: 1, 1000: 1, 1500: 1, 2000: 1},
-          }
+    cfg = {
+        "GENERAL": {"user": "ncb", "folder": str(home), "save_directory": str(home / "data"), "python_exe": sys.executable},
+        "HEAD": {},
+        "ATTENUATION": {
+            -1: 1,
+            0: 1,
+            100: 1,
+            150: 1,
+            200: 1,
+            250: 1,
+            300: 1,
+            350: 1,
+            400: 1,
+            450: 1,
+            500: 1,
+            600: 1,
+            700: 1,
+            800: 1,
+            900: 1,
+            1000: 1,
+            1500: 1,
+            2000: 1,
+        },
+    }
 
-    paths = {'playlistfolder': 'ethoconfig/playlists',
-            'protocolfolder': 'ethoconfig/protocols',
-            'stimfolder': 'ethoconfig/stim',
-            'datafolder': 'data'}
-    logging.info('Creating default directories:')
+    paths = {
+        "playlistfolder": "ethoconfig/playlists",
+        "protocolfolder": "ethoconfig/protocols",
+        "stimfolder": "ethoconfig/stim",
+        "datafolder": "data",
+    }
+    logging.info("Creating default directories:")
     for name, path in paths.items():
-        logging.info('   ' + path)
+        logging.info("   " + path)
         p = home / Path(path)
         p.mkdir(parents=True, exist_ok=True)
-        cfg['HEAD'][name] = str(p)
+        cfg["HEAD"][name] = str(p)
 
-    logging.info(cfg)
+    rich.print(cfg)
 
-    path_cfg = home / 'ethoconfig.yml'
+    path_cfg = home / "ethoconfig.yml"
     if path_cfg.exists():
-        logging.info(f'The configuration file {str(path_cfg)} exists. Will not overwrite. You may have to update the file manually')
+        logging.info(
+            f"The configuration file {str(path_cfg)} exists. Will not overwrite. You may have to update the file manually"
+        )
     else:
-        logging.info(f'Writing configuration to {str(path_cfg)}.')
-        with open(path_cfg, mode='w') as f:
+        logging.info(f"Writing configuration to {str(path_cfg)}.")
+        with open(path_cfg, mode="w") as f:
             yaml.dump(cfg, f, Dumper=yaml.SafeDumper)
 
-    logging.info('Generating test files:')
+    logging.info("Generating test files:")
     # generate test protocol
-    protocol = {'NODE': {'maxduration': 30, 'use_services': ['GCM']},
-            'GCM': {'frame_rate': 30,
-            'frame_width': 320,
-            'frame_height': 240,
-            'frame_offx': 0,
-            'frame_offy': 0,
-            'shutter_speed': 1.0,
-            'brightness': 1.0,
-            'gamma': 1.0,
-            'gain': 1.0,
-            'cam_serialnumber': 42,
-            'cam_type': 'Dummy',
-            'callbacks': {'disp': {'framerate': 10}}}}
-    path_protocol = home / 'ethoconfig/protocols/dummy_1min.yml'
-    logging.info(f'   protocol {str(path_protocol)}.')
+    protocol = {
+        "NODE": {"maxduration": 30, "use_services": ["GCM"]},
+        "GCM": {
+            "frame_rate": 30,
+            "frame_width": 320,
+            "frame_height": 240,
+            "frame_offx": 0,
+            "frame_offy": 0,
+            "shutter_speed": 1.0,
+            "brightness": 1.0,
+            "gamma": 1.0,
+            "gain": 1.0,
+            "cam_serialnumber": 42,
+            "cam_type": "Dummy",
+            "callbacks": {"disp": {"framerate": 10}},
+        },
+    }
+    path_protocol = home / "ethoconfig/protocols/dummy_1min.yml"
+    logging.info(f"   protocol {str(path_protocol)}.")
 
-    with open(path_protocol, mode='w') as f:
-            yaml.dump(protocol, f, Dumper=yaml.SafeDumper)
+    with open(path_protocol, mode="w") as f:
+        yaml.dump(protocol, f, Dumper=yaml.SafeDumper)
 
     # generate test playlist
-    playlist = {'stimFileName': {0: 'SIN_100_0_1000'},
-                'silencePre': {0: 10000},
-                'silencePost': {0: 9000},
-                'delayPost': {0: 0},
-                'intensity': {0: 0.0},
-                'freq': {0: 100},
-                'MODE': {0: None}}
-    path_playlist = home / 'ethoconfig/playlists/0_silence.txt'
-    logging.info(f'   playlist {str(path_playlist)}.')
-    pd.DataFrame.from_dict(playlist).to_csv(path_playlist, sep='\t', index=False)
+    playlist = {
+        "stimFileName": {0: "SIN_100_0_1000"},
+        "silencePre": {0: 10000},
+        "silencePost": {0: 9000},
+        "delayPost": {0: 0},
+        "intensity": {0: 0.0},
+        "freq": {0: 100},
+        "MODE": {0: None},
+    }
+    path_playlist = home / "ethoconfig/playlists/0_silence.txt"
+    logging.info(f"   playlist {str(path_playlist)}.")
+    pd.DataFrame.from_dict(playlist).to_csv(path_playlist, sep="\t", index=False)
 
 
 def main():
@@ -157,6 +190,7 @@ def main():
 
     try:
         from .gui import app
+
         subcommands["gui"] = app.main
     except (ImportError, ModuleNotFoundError):
         logging.exception("No GUI avalaible.")
