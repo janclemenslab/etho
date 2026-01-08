7b8f2c1ddba46395691ce68c5e29e84d224a16c3
Jan Clemens <clemensjan@googlemail.com>
2026-01-08 15:37:57 +0100

    extend and clean up tests

---
+++ tests/test_import.py
@@ -0,0 +1,7 @@
+def test_imports():
+    import etho
+    import etho.app
+    import etho.cli
+    import etho.client
+    import etho.services
+    import etho.utils
--- tests/test_sound.py
+++ tests/test_sound.py
@@ -1,10 +1,10 @@
 import pandas as pd
 from etho.utils.sound import parse_table, load_sounds
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 
 def test_playlist():
-    plt.ion()
+    # plt.ion()
     for playlistfile in ['tests/test_sound_playlist_mono.txt', 'tests/test_sound_playlist_mirrorled.txt', 'tests/test_sound_playlist_stereo.txt']:
         print(playlistfile)
         df = parse_table(playlistfile, [str, float, float, float, float, float, str])
@@ -13,10 +13,10 @@ def test_playlist():
         with pd.option_context('display.max_rows', None, 'display.max_columns', None, 'display.width', 1000):
             print(df)
         sounds = load_sounds(df, fs=10000)
-        plt.clf()
-        for chn in range(sounds[0].shape[1]):
-            plt.subplot(sounds[0].shape[1], 1, chn+1)
-            plt.plot(sounds[0][:, chn])
-        plt.title(playlistfile)
-        plt.show()
-        plt.pause(1)
+        # plt.clf()
+        # for chn in range(sounds[0].shape[1]):
+        #     plt.subplot(sounds[0].shape[1], 1, chn+1)
+        #     plt.plot(sounds[0][:, chn])
+        # plt.title(playlistfile)
+        # plt.show()
+        # plt.pause(1)
