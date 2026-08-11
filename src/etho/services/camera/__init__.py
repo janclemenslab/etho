e828b81c9915033dc2d5060c0d4817413ee90833
Jan Clemens <clemensjan@googlemail.com>
2026-08-11 10:47:05 +0200

    add spinnaker legacy cam support

--- src/etho/services/camera/__init__.py
+++ src/etho/services/camera/__init__.py
@@ -1,10 +1,12 @@
 from .spinnaker import Spinnaker
+from .spinnaker_old import Spinnaker_OLD
 from .ximea import Ximea
 from .basler import Basler
 from .hamamatsu import Hamamatsu
 from .dummy import Dummy
 
 make = {
+    "Spinnaker_OLD": Spinnaker_OLD,
     "Spinnaker": Spinnaker,
     "Ximea": Ximea,
     "Basler": Basler,
